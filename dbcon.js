35a48548b7fc6182ac8b0408c551045c7031707b
jdaccarett <jdaccarett101@gmail.com>
2017-10-30 09:17:03 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -4,10 +4,10 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
 
     connectionLimit : 10,
-    host            : 'localhost',
-    user            : 'root',
-    password        : 'weeman91',
-    database        : 'fitness'
+    host            : '',
+    user            : '',
+    password        : '',
+    database        : ''
 
 });
 
